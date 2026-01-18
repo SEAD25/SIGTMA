f1b88b53d2a08ed3f8eaad7f43e93767362ed6bb
DiengAliou <ad380914@gmail.com>
2026-01-18 11:08:24 +0000

    version 1.3

--- src/main/java/sn/uasz/SIGTMA/SigtmaApplication.java
+++ src/main/java/sn/uasz/SIGTMA/SigtmaApplication.java
@@ -1,13 +1,20 @@
 package sn.uasz.SIGTMA;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SigtmaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SigtmaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SigtmaApplication.class, args);
+    }
 
+    // Ce bean est nécessaire pour éviter l'erreur dans votre Controller
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
